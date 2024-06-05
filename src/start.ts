6d92ceff48edabbb02ad444c491524466a335d11
Yang Li <cyberyoung@users.noreply.github.com>
2024-06-05 16:00:58 +0800

    fix host config

--- src/start.ts
+++ src/start.ts
@@ -35,7 +35,7 @@ export async function start({
         : 'npx vite'
     }
 
-    command += ` --port=${ctx.port} --host=${ctx.host}`
+    command += ` --port=${ctx.port} --host=${Config.host}`
 
     executeCommand(command)
   }
